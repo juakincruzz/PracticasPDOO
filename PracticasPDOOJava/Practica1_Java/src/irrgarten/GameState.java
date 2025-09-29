2ec2905cd0c48929d528076aa9b90f4be05b4f26
Joaquín José Cruz Lorenzo <113943481+juakincruzz@users.noreply.github.com>
2025-09-29 19:11:04 +0200

    Add files via upload

---
+++ PracticasPDOOJava/Practica1_Java/src/irrgarten/GameState.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package irrgarten;
+
+/**
+ *
+ * @author joaquin
+ */
+public class GameState {
+    private final String labyrinth; // Representacion del laberinto.
+    private final String players;   // Representacion de los jugadores.
+    private final String monsters; // Representacion de los monstruos.
+    private final int currentPlayer; // Indice del jugador actual.
+    private final boolean winner; // Ganador?
+    private final String log; // Mensajes recientes.
+
+    public GameState(String labyrinth, String players, String monsters, int currentPlayer, boolean winner, String log) {
+        this.labyrinth = labyrinth;
+        this.players = players;
+        this.monsters = monsters;
+        this.currentPlayer = currentPlayer;
+        this.winner = winner;
+        this.log = log;
+    }
+
+    public int getCurrentPlayer() { return currentPlayer; }
+    public String getLabyrinth() { return labyrinth; }
+    public String getPlayers() { return players; }
+    public String getMonsters() { return monsters; }
+    public boolean isWinner() { return winner; }
+    public String getLog() { return log; }
+
+    @Override
+    public String toString() {
+        return "GameState{" + "labyrinth=" + labyrinth + ", players=" + players + ", monsters=" + monsters + 
+                    ", currentPlayer=" + currentPlayer + ", winner=" + winner + ", log=" + log + '}';
+    }
+}
---
+++ PracticasPDOOJava/Practica1_Java/src/irrgarten/Shield.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package irrgarten;
+
+/**
+ *
+ * @author joaquin
+ */
+public class Shield {
+    // Atributos de instancia privados
+    private float protection;
+    private int uses;
+    
+    // Constructor
+    public Shield(float protection, int uses) {
+        this.protection = protection;
+        this.uses = uses;
+    }
+    
+    public float protect(){
+        if (uses > 0){
+            uses--;
+            return protection;
+        }
+        
+        return 0.0f;
+    }
+
+    @Override
+    public String toString() {
+        return "S[" + "protection=" + protection + ", uses=" + uses + ']';
+    }
+    
+    public boolean discard() {
+        return Dice.discardElement(uses);
+    }
+}

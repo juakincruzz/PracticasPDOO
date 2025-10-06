bf96fb91c96a20f6f072a8ef48d1c26f3e13b903
Joaquín José Cruz Lorenzo <113943481+juakincruzz@users.noreply.github.com>
2025-10-06 12:43:39 +0200

    Add files via upload

---
+++ PracticasPDOORuby/directions.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Irrgarten
+  module Directions
+    LEFT = :left
+    RIGHT = :right
+    UP = :up
+    DOWN = :down
+    ALL = [LEFT, RIGHT, UP, DOWN].freeze
+  end
+end

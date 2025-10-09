dd3577dbdca059d0aa37e05d752c72cc13e82081
juakincruzz <113943481+juakincruzz@users.noreply.github.com>
2025-10-09 16:33:43 +0200

    FIN DE IMPLEMENTACIÓN EN P2_RUBY, QUEDA PENDIENTE EL TESTP2 MAS LA DOCUMENTACION Y PONER LOS METODOS PRIVADOS Y PUBLICOS

--- PracticasPDOORuby/Practica2_Ruby/lib/Irrgarten/game.rb
+++ PracticasPDOORuby/Practica2_Ruby/lib/Irrgarten/game.rb
@@ -1,7 +1,82 @@
 # frozen_string_literal: true
+require_relative 'player'
+require_relative 'monster'
+require_relative 'labyrinth'
+require_relative 'game_state'
+require_relative 'dice'
+
 module Irrgarten
   class Game
+    MAX_ROUNDS = 10
 
+    def initialize(nplayers)
+      @players = Array.new(nplayers) { |i| Player.new(i, Dice.random_intelligence, Dice.random_strength) }
+      @monsters = []
+      @log = ""
+      @labyrinth = Labyrinth.new(10, 10, 0, 0)
+      @current_player_index = Dice.who_starts(nplayers)
+
+      # @labyrinth.spread_players(@players)
+    end
+
+    def finished?
+      @labyrinth.have_a_winner?
+    end
+
+    def get_game_state
+      GameState.new(@labyrinth.to_s, @players.map(&:to_s).join("\n"), @monsters.map(&:to_s).join("\n"), @current_player_index, finished?, @log)
+    end
+
+    def configure_labyrinth
+      # Añadir algunos monstruos de ejemplo
+      5.times do |i|
+        monster = Monster.new("Monster#{i}", Dice.random_intelligence, Dice.random_strength)
+        pos = @labyrinth.random_empty_pos
+        @labyrinth.add_monster(pos[Labyrinth::ROW], pos[Labyrinth::COL], monster)
+        @monsters << monster
+      end
+    end
+
+    def next_player
+      @current_player_index = (@current_player_index + 1) % @players.length
+    end
+
+    def log_player_won
+      @log += "El jugador ha ganado la partida!\n"
+    end
+
+    def log_monster_won
+      @log += "El monstruo ha ganado la partid!\n"
+    end
+
+    def log_resurrected
+      @log += "El jugador ha resucitado\n"
+    end
+
+    def log_player_skip_turn
+      @log += "El jugador pierde el turno por estar muerto\n"
+    end
+
+    def log_player_no_orders
+      @log += "El jugador no siguio las indicaciones\n"
+    end
+
+    def log_no_monster
+      @log += "El jugador se ha movido a una casilla vacia o no le ha sido posible moverse\n"
+    end
+
+    def log_rounds(rounds, max)
+      @log += "Ronda #{rounds} de #{max}\n"
+    end
+
+    # ===========================================================
+    # P3
+    # ===========================================================
+    def next_step(preferred_direction) = raise NotImplementedError, "P3"
+    def actual_direction(preferred_direction) = raise NotImplementedError, "P3"
+    def combat(monster) = raise NotImplementedError, "P3"
+    def manage_reward(winner) = raise NotImplementedError, "P3"
+    def manage_resurreection = raise NotImplementedError, "P3"
   end
 end
 
--- PracticasPDOORuby/Practica2_Ruby/lib/Irrgarten/labyrinth.rb
+++ PracticasPDOORuby/Practica2_Ruby/lib/Irrgarten/labyrinth.rb
@@ -12,6 +12,90 @@ module Irrgarten
     EXIT_CHAR = 'E'
     ROW = 0
     COL = 1
+
+    def initialize(n_rows, n_cols, exit_row, exit_col)
+      @n_rows = n_rows
+      @n_cols = n_cols
+      @exit_row = exit_row
+      @exit_col = exit_col
+
+      @monsters = Array.new(n_rows) { Array.new(n_cols) }
+      @players = Array.new(n_rows) { Array.new(n_cols) }
+      @labyrinth = Array.new(n_rows) { Array.new(c_cols, EMPTY_CHAR) }
+      @labyrinth[exit_row][exit_col] = EXIT_CHAR
+    end
+
+    def have_a_winner?
+      !@players[@exit_row][@exit_col].nil?
+    end
+
+    def to_s
+      @labyrinth.map { |row| row.join("\t") }.join("\n")
+    end
+
+    def add_monster(row, col, monster)
+      if pos_ok(row, col) && empty_pos(row, col)
+        @labyrinth[row][col] = MONSTER_CHAR
+        @monsters[row][col] = monster
+        monster.set_pos(row, col)
+      end
+    end
+
+    def pos_ok?(row, col)
+      row.between?(0, @n_rows - 1) && col.between?(0, @n_cols - 1)
+    end
+
+    def empty_pos?(row, col)
+      pos_ok?(row, col) && @labyrinth[row][col] == EMPTY_CHAR
+    end
+
+    def monster_pos(row, col)
+      pos_ok?(row, col) && @labyrinth[row][col] == MONSTER_CHAR
+    end
+
+    def exit_pos?(row, col)
+      pos_ok?(row, col) && @labyrinth[row][col] == EXIT_CHAR
+    end
+
+    def combat_pos?(row, col)
+      pos_ok?(row, col) && @labyrinth[row][col] == COMBAT_CHAR
+    end
+
+    def can_step_on?(row, col)
+      pos_ok?(row, col) && empty_pos?(row, col) || combat_pos?(row, col) || exit_pos?(row, col)
+    end
+
+    def update_old_pos(row, col)
+      if pos_ok?(row, col)
+        @labyrinth[row][col] = combat_pos?(row, col) ? MONSTER_CHAR : EXIT_CHAR
+      end
+    end
+
+    def dir_2_pos(row, col, direction)
+      case direction
+      when Directions::UP then [row - 1, col]
+      when Directions::DOWN then [row + 1, col]
+      when Directions::LEFT then [row, col - 1]
+      when Directions::RIGHT then [row, col + 1]
+      else [row, col]
+      end
+    end
+
+    def random_empty_pos
+      loop do
+        row = Dice.random_pos(@n_rows)
+        col = Dice.random_pos(@n_cols)
+        return [row, col] if empty_pos?(row, col)
+      end
+    end
+
+    # ======================================================
+    # P3
+    # ======================================================
+    def spread_player(players) = raise NotImplementedError, "P3"
+    def put_player(direction, player) = raise NotImplementedError, "P3"
+    def addBlock(orientation, start_row, start_col, length) = raise NotImplementedError, "P3"
+    def valid_moves(row, col) = raise NotImplementedError, "P3"
   end
 end
 
